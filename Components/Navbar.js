977244ec9f55e1df92794bf282f0319bc8e8be4a
Harsh Kaushik <harshkaushik494@gmail.com>
2026-08-08 11:52:22 +0530

    Frontend design

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,16 +1,25 @@
 import React from 'react'
-
+import Link from 'next/link'
 const Navbar = () => {
   return (
     <nav className="bg-gray-950 text-white flex justify-between px-4 h-16 items-center">
-      <div className="logo font-bold text-lg ">GetMeaChai !</div>
-      <ul className='flex justify-between gap-4'>
+      <div className="logo font-bold text-lg flex justify-center items-center ">
+        <img src="/chai.gif" width={34} alt="" />
+        <span>Get Me a Chai !</span>
+        </div>
+      {/* <ul className='flex justify-between gap-4'>
         <li>Home</li>
         <li>About</li>
         <li>Projects</li>
         <li>Sign Up</li>
         <li>Login </li>
-      </ul>
+      </ul> */}
+
+      <div>
+        <Link href="/login">
+          <button type="button" className="text-white bg-gradient-to-br rounded-lg from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Login</button>
+        </Link>
+      </div>
     </nav>
   )
 }
--- app/page.js
+++ app/page.js
@@ -17,12 +17,36 @@ export default function Home() {
       <div className="bg-white h-1 opacity-10">
       </div>
 
-      <div>
-        <h1>Your Fans can buy you a Chai </h1>
-        <div className="flex gap-5">
-          <div
+      <div className="text-white container mx-auto py-32 pt-14">
+        <h2 className="text-3xl font-bold text-center mb-14">Your Fans can buy you a Chai </h2>
+        <div className="flex gap-5 justify-around">
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/man.gif" alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className=" text-center"> Your fans are available for you to help you</p>
+          </div>
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/coin.gif" alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className=" text-center"> Your fans are available for you to help you</p>
+          </div>
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/group.gif" alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className=" text-center"> Your fans are available for you to help you</p>
+          </div>
         </div>
       </div>
+
+      <div className="bg-white h-1 opacity-10">
+      </div>
+
+      <div className="text-white container mx-auto py-32 pt-14 flex flex-col items-center justify-center">
+        <h2 className="text-3xl font-bold text-center mb-14">Learn more about us </h2>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/QtaorVNAwbI?si=uhmCHZjzCA21FaOc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      </div>
+
+      
       </>
   );
 }
